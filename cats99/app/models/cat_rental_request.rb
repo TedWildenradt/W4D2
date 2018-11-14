d30423118a9cc7e2ec86a59a7d069a8adeefe9eb
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-13 17:45:18 -0800

    Refactor _forms

--- cats99/app/models/cat_rental_request.rb
+++ cats99/app/models/cat_rental_request.rb
@@ -4,7 +4,6 @@ class CatRentalRequest < ApplicationRecord
   validates :cat_id, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :cat_id, presence: true
   validates :status, presence: true, inclusion: {in: STATUS}
 
   belongs_to :cat,
